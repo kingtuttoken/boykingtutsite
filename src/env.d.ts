df8d174c6f9d6f639c9118157ca65bb8dbae10fb
kingtuttoken <33alienguru@gmail.com>
2026-08-22 21:33:35 -0400

    Add files via upload

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,4 @@
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
-
-declare namespace App {
-  interface Locals extends Runtime {}
+/// <reference types="astro/client" />
+declare module "cloudflare:workers" {
+  export const env: Record<string, any>;
 }
